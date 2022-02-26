b6dbfd458e1b3b3215572206b33a818844e64998
Ana Muraya <anamuraya9@gmail.com>
2022-02-26 01:30:47 -0800

    Added the php templates plus worked on the database

--- functions.php
+++ functions.php
@@ -1,18 +1,19 @@
 <?php
-//    require MySQL Connection
+
+// require MySQL Connection
 require ('database/DBController.php');
 
-//    require Product Class
+// require Product Class
 require ('database/Product.php');
 
 // require Cart Class
 require ('database/Cart.php');
 
 
-//DBController object
+// DBController object
 $db = new DBController();
 
-//Product object
+// Product object
 $product = new Product($db);
 $product_shuffle = $product->getData();
 
